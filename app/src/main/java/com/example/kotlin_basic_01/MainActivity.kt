c98224f71671218aa21050a79fd300ec32c3312f
Joo <rudwn3166@naver.com>
2021-08-23 17:38:12 +0900

    fhr로그 찍어보기

--- app/src/main/java/com/example/kotlin_basic_01/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_basic_01/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.kotlin_basic_01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +10,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        clickBtn.setOnClickListener {
+
+//            setOnClickListener는 버튼이 눌렸을 때 기능을 수행해주는 기능
+//
+            Log.d("메인화면로그","클릭용 버튼 눌림")
+        }
 
     }
 }
